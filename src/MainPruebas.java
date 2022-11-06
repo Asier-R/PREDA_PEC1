2c4b48181320028903b567a22306c6b07f7e8dca
Asier <monstruosaurio@gmail.com>
2022-11-06 12:15:11 +0100

    Borrado ficheros prueba, lectura de ficheros y refactorizado

--- src/MainPruebas.java
+++ src/MainPruebas.java
@@ -1,48 +1,92 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainPruebas {
     public static void main(String[] args) {
+        System.out.println(System.getProperty("user.home"));
+        String archivo = System.getProperty("user.dir")+"\\FICHEROS\\"+"ficheroPruebas.txt";
+        File fichero = new File(archivo);
 
-        pruebas_de_ficheros();
-
+        pruebas_de_ficheros(fichero);
+        validarDatos(leerFichero(fichero));
 
     }
 
-    static void pruebas_de_ficheros(){
-        File fichero = new File(System.getProperty("user.dir")+"\\FICHEROS\\"+"ficheroPruebas.txt");
+    static void validarDatos(String datos){
+        System.out.println("SYSTEM: inicio de la validación de los datos.");
+        String[] arrayDatos = datos.split("\n");
+
+        //Número de objetos
+        String regex = "^[0-9]+$";
+        Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        Matcher matcher = pattern.matcher(arrayDatos[0]);
+
+        if(matcher.find())
+            System.out.println("SYSTEM: el número de objetos es "+arrayDatos[0]+".");
+        else
+            System.out.println("ERROR: no se ha introducido el número de objetos.");
+
+        //Capacidad de la mochila
+        regex = "^[0-9]+$";
+        pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        matcher = pattern.matcher(arrayDatos[arrayDatos.length-1]);
+
+        if(matcher.find())
+            System.out.println("SYSTEM: la capacidad de la mochila es "+arrayDatos[arrayDatos.length-1]+".");
+        else
+            System.out.println("ERROR: no se ha introducido la capacidad de la mochila.");
+
+        //El número de objetos coincide con lo indicado en la primera línea
+        if(arrayDatos.length-2 == Integer.parseInt(arrayDatos[0]))
+            System.out.println("SYSTEM: el número de objetos es coherente con lo indicado.");
+        else
+            System.out.println("ERROR: el número de objetos no cuadra con lo indicado.");
+
+        //Comprobar validez de objetos
+        regex = "^[0-9]+ +[0-9]+$";
+        pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        matcher = pattern.matcher(arrayDatos[arrayDatos.length-1]);
+
+        for(int i=1; i< arrayDatos.length-2; i++){
+            matcher = pattern.matcher(arrayDatos[i]);
+            if(!matcher.find())
+                System.out.println("ERROR: uno de los objetos no tiene el formato correcto.");
+        }
+
+        System.out.println("SYSTEM: fin de validación de los datos.");
+    }
+
+    static String leerFichero(File fichero){
+        String datos = "";
+        try {
+            Scanner lector = new Scanner(fichero);
+
+            while(lector.hasNext())
+                datos+=lector.nextLine()+"\n";
+
+            System.out.println("LECTURA: \n"+datos);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        return datos;
+    }
+
+    static void pruebas_de_ficheros(File fichero){
+
         System.out.println("Path => "+fichero.getPath());
         System.out.println("Existe fichero => "+fichero.exists());
         System.out.println("Es fichero => "+fichero.isFile());
-        System.out.println("........................................................................................");
 
-        File fichero2 = new File(System.getProperty("user.dir")+"\\FICHEROS\\"+"ficheroVacio.txt");
-        System.out.println("Path => "+fichero2.getPath());
-        System.out.println("Existe fichero => "+fichero2.exists());
-        System.out.println("Es fichero => "+fichero2.isFile());
-        System.out.println("........................................................................................");
-
-        File fichero3 = new File(System.getProperty("user.dir")+"\\FICHEROS\\"+"JG1.jpg");
-        System.out.println("Path => "+fichero3.getPath());
-        System.out.println("Existe fichero => "+fichero3.exists());
-        System.out.println("Es fichero => "+fichero3.isFile());
-        System.out.println("........................................................................................");
-
-        File fichero4 = new File(System.getProperty("user.dir")+"\\FICHEROS\\"+"carpetaFichero");
-        System.out.println("Path => "+fichero4.getPath());
-        System.out.println("Existe fichero => "+fichero4.exists());
-        System.out.println("Es fichero => "+fichero4.isFile());
-        System.out.println("........................................................................................");
-
-        File fichero5 = new File(System.getProperty("user.dir")+"\\FICHEROS\\"+"Nuevo Pres.odp");
-        System.out.println("Path => "+fichero5.getPath());
-        System.out.println("Existe fichero => "+fichero5.exists());
-        System.out.println("Es fichero => "+fichero5.isFile());
-        System.out.println("........................................................................................");
-
-        File fichero6 = new File(System.getProperty("user.dir")+"\\FICHEROS\\"+"Nuevo Texto.odt");
-        System.out.println("Path => "+fichero6.getPath());
-        System.out.println("Existe fichero => "+fichero6.exists());
-        System.out.println("Es fichero => "+fichero6.isFile());
-        System.out.println("........................................................................................");
+        System.out.println("File name: " + fichero.getName());
+        System.out.println("Absolute path: " + fichero.getAbsolutePath());
+        System.out.println("Writeable: " + fichero.canWrite());
+        System.out.println("Readable " + fichero.canRead());
+        System.out.println("File size in bytes " + fichero.length());
     }
 }
--- src/mochila_voraz.java
+++ src/mochila_voraz.java
@@ -1,4 +1,8 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * PEC1
@@ -27,7 +31,7 @@ public class mochila_voraz {
 
         }
         else{
-            comprobarArgumentos(args);
+            if(!comprobarArgumentos(args)) FINDEPROGAMA = true;
         }
 
         if (FINDEPROGAMA){
@@ -44,37 +48,22 @@ public class mochila_voraz {
         System.out.println(" fichero_salida: es el nombre del fichero que se creará para almacenar la salida.\n\n");
     }
 
-    static void comprobarArgumentos(String[] args){
+    static boolean comprobarArgumentos(String[] args){
         //Se utilizan 4 argumentos
         if(args.length == 4){
             //Primer argumento
-            if(!esValidoArgumento(args[0])){
-                FINDEPROGAMA = true;
-                return;
-            }
+            if(!esValidoArgumento(args[0])) return false;
             //Segundo argumento
-            if(!esValidoArgumento(args[1])){
-                FINDEPROGAMA = true;
-                return;
-            }
+            if(!esValidoArgumento(args[1])) return false;
             //Tercer argumento
-            if(!esValidoArgumentoFichero(args[2], true)){
-                FINDEPROGAMA = true;
-                return;
-            }
+            if(!esValidoArgumentoFichero(args[2], true)) return false;
             //Cuarto argumento
-            if(!esValidoArgumentoFichero(args[3], false)){
-                FINDEPROGAMA = true;
-                return;
-            }
+            return esValidoArgumentoFichero(args[3], false);
         }
         //Se utilizan 3 argumentos
         else if(args.length == 3){
             //Primer argumento
-            if(!esValidoArgumento(args[0])){
-                FINDEPROGAMA = true;
-                return;
-            }
+            if(!esValidoArgumento(args[0])) return false;
             //Segundo argumento
             boolean esEntrada = false;
             switch (args[1]){
@@ -86,19 +75,12 @@ public class mochila_voraz {
                     mostrarAyuda();
                     break;
                 default:
-                    System.out.println("SYSTEM: se comprueba validez del fichero '"+args[1]+"'.");
-                    if(!validarFichero(args[1], true)){
-                        FINDEPROGAMA = true;
-                        return;
-                    }
+                    if(!validarFichero(args[1], true)) return false;
                     esEntrada = true; //Para indicar si el tercer argumento es fichero de salida
                     break;
             }
             //Tercer argumento
-            if(!esValidoArgumentoFichero(args[2], !esEntrada)){
-                FINDEPROGAMA = true;
-                return;
-            }
+            return esValidoArgumentoFichero(args[2], !esEntrada);
         }
         //Se utilizan 2 argumentos
         else if(args.length == 2){
@@ -113,21 +95,14 @@ public class mochila_voraz {
                     mostrarAyuda();
                     break;
                 default:
-                    System.out.println("SYSTEM: se comprueba validez del fichero '"+args[0]+"'.");
-                    if(!validarFichero(args[0], true)){
-                        FINDEPROGAMA = true;
-                        return;
-                    }
+                    if(!validarFichero(args[0], true)) return false;
                     esEntrada = true; //Para indicar si el segundo argumento es fichero de salida
                     break;
             }
             //Segundo argumento
-            if(!esValidoArgumentoFichero(args[2], !esEntrada)){
-                FINDEPROGAMA = true;
-                return;
-            }
+            return esValidoArgumentoFichero(args[2], !esEntrada);
         }
-        //Se utilizan 2 argumentos
+        //Se utilizan 1 argumento
         else if(args.length == 1){
             switch (args[0]){
                 case "-t":
@@ -138,15 +113,13 @@ public class mochila_voraz {
                     mostrarAyuda();
                     break;
                 default:
-                    System.out.println("SYSTEM: se comprueba validez del fichero '"+args[0]+"'.");
-                    if(!validarFichero(args[0], true)){
-                        FINDEPROGAMA = true;
-                        return;
-                    }
-                    break;
+                    return validarFichero(args[0], true);
             }
+            return true;
         }
-
+        else{
+            return false;
+        }
     }
 
     static boolean esValidoArgumento(String arg){
@@ -169,19 +142,102 @@ public class mochila_voraz {
         if(arg.equals("-t") || arg.equals("-h")){
             System.out.println("ERROR: argumento "+arg+" no válido.");
             mostrarAyuda();
+            FINDEPROGAMA = true;
             return false;
         }else{
             System.out.println("SYSTEM: se comprueba validez del fichero '"+arg+"'.");
-            if(!validarFichero(arg, esEntrada)) {
-                return false;
-            }
+            return validarFichero(arg, esEntrada);
         }
-        return true;
     }
 
     static boolean validarFichero(String nombre_fichero, Boolean esEntrada){
+        System.out.println("SYSTEM: inicio de validación de fichero "+nombre_fichero+".");
         File fichero = new File(nombre_fichero);
-        return fichero.isFile();
+        //¿Existe fichero?
+        if(!fichero.exists()) {
+            System.out.println("ERROR: el fichero no existe.");
+            FINDEPROGAMA = true;
+            return false;
+        }
+        //¿Es fichero válido?
+        if(!fichero.isFile()) {
+            System.out.println("ERROR: no es un fichero válido.");
+            FINDEPROGAMA = true;
+            return false;
+        }
+        //¿Se puede leer?
+        if(!fichero.canRead()) {
+            System.out.println("ERROR: el fichero no se puede leer.");
+            FINDEPROGAMA = true;
+            return false;
+        }
+        //Fichero válido
+        return true;
+    }
+
+    static void validarDatos(String[] arrayDatos){
+        System.out.println("SYSTEM: inicio de la validación de los datos.");
+
+        //Número de objetos
+        String regex = "^[0-9]+$";
+        Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        Matcher matcher = pattern.matcher(arrayDatos[0]);
+
+        if(matcher.find())
+            System.out.println("SYSTEM: el número de objetos es "+arrayDatos[0]+".");
+        else
+            System.out.println("ERROR: no se ha introducido el número de objetos.");
+
+        //Capacidad de la mochila
+        regex = "^[0-9]+$";
+        pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        matcher = pattern.matcher(arrayDatos[arrayDatos.length-1]);
+
+        if(matcher.find())
+            System.out.println("SYSTEM: la capacidad de la mochila es "+arrayDatos[arrayDatos.length-1]+".");
+        else
+            System.out.println("ERROR: no se ha introducido la capacidad de la mochila.");
+
+        //El número de objetos coincide con lo indicado en la primera línea
+        if(arrayDatos.length-2 == Integer.parseInt(arrayDatos[0]))
+            System.out.println("SYSTEM: el número de objetos es coherente con lo indicado.");
+        else
+            System.out.println("ERROR: el número de objetos no cuadra con lo indicado.");
+
+        //Comprobar validez de objetos
+        regex = "^[0-9]+ +[0-9]+$";
+        pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        matcher = pattern.matcher(arrayDatos[arrayDatos.length-1]);
+
+        for(int i=1; i< arrayDatos.length-2; i++){
+            matcher = pattern.matcher(arrayDatos[i]);
+            if(!matcher.find())
+                System.out.println("ERROR: uno de los objetos no tiene el formato correcto.");
+        }
+
+        System.out.println("SYSTEM: fin de validación de los datos.");
+    }
+
+    static String leerFichero(String path){
+        //Obtenemos el fichero
+        String archivo = System.getProperty(path);
+        File fichero = new File(archivo);
+
+        String datos = "";
+        try {
+            Scanner lector = new Scanner(fichero);
+
+            while(lector.hasNext())
+                datos+=lector.nextLine()+"\n";
+
+            System.out.println("LECTURA: \n"+datos);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        return datos;
     }
 
 }
