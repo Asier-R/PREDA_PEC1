06af809a924079e0304b5a7a055fe053d2c7707f
Asier <monstruosaurio@gmail.com>
2022-11-14 19:36:07 +0100

    Se finaliza entrada y se completa excel casos de prueba

--- src/MainPruebas.java
+++ src/MainPruebas.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,12 +16,31 @@ public class MainPruebas {
         validarDatos(leerFichero(fichero));*/
 
 
-
+        String fichero = "C:\\Users\\Monst\\Desktop\\UNED\\Asignaturas\\2022-2023\\Programacion y Estructuras de Datos Avanzadas\\PRACTICA 1\\PREDA_PEC1\\FICHEROS\\ficheroPruebas.txt";
 
         ArrayList<String[]> casosPrueba = new ArrayList<>(100);
-        casosPrueba.add(new String[] {"-t"});
 
-        casosPrueba.forEach(caso -> mochila_voraz.main(caso));
+        casosPrueba.add(new String[] {fichero,fichero,"-t","-t"});
+        casosPrueba.add(new String[] {fichero,fichero,fichero,"-t"});
+        casosPrueba.add(new String[] {fichero,fichero,"-h",fichero});
+        casosPrueba.add(new String[] {fichero,fichero,fichero,fichero});
+
+        casosPrueba.add(new String[] {"-t",fichero,"-t","-t"});
+        casosPrueba.add(new String[] {fichero,"-h","-t","-t"});
+        casosPrueba.add(new String[] {"-t","-h","-t","-t"});
+
+
+
+
+        casosPrueba.forEach(caso -> {
+            System.out.println("CASO: "+ Arrays.stream(caso).toList().toString());
+            mochila_voraz.main(caso);
+            mochila_voraz.existeFicheroEntrada = false;
+            mochila_voraz.existeFicheroSalida = false;
+            mochila_voraz.trazasActivas = false;
+            mochila_voraz.FINDEPROGAMA = false;
+            System.out.println("-----------------------------------------------------------------------------------");
+        });
 
     }
 
